745d9ff674aa909310161f452ac285955c9b41d2
César Henrique de Figueiredo <cesar.sprite@gmail.com>
2025-05-09 20:06:41 -0300

    feat: dotenv config

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+require("dotenv").config();
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -7,9 +8,10 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
+      host: process.env.DB_HOST,
       database: "xpe",
-      user: process.env.USERNAME,
-      password: process.env.PASSWORD,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
     },
     migrations: {
       tableName: "knex_migrations",
--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,8 @@
 const knex = require("knex");
 const config = require("../../knexfile");
 
+console.log(config);
+
 const db = knex(config.development);
 
 module.exports = db;
